d74abd14d109f67f32a75af1f0ce06b3a2172458
aidarbek <aidarbekabdyrakmanov@gmail.com>
2023-03-08 13:26:31 +0600

    add applic

---
+++ src/main/java/enc/encryp_api/EncrypApiApplication.java
@@ -0,0 +1,13 @@
+package enc.encryp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncrypApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncrypApiApplication.class, args);
+    }
+
+}
